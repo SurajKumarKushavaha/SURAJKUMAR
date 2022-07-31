cc59d3bcf7462d14d7fa7fa2e767f1eac1d9ed01
SurajKumarKushavaha <52530381+SurajKumarKushavaha@users.noreply.github.com>
2022-07-31 08:47:06 +0530

    Add files via upload

---
+++ Test.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	/*int HH,MM;
+	printf("Enter Time Format (HH:MM): ");
+	scanf("%d:%d",&HH,&MM);
+	printf("%d Hour and %d Minutes ",HH,MM);*/
+	/*int DD,MM,YYYY;
+	printf("Enter Date Format (DD/MM/YYYY): ");
+	scanf("%d/%d/%d",&DD,&MM,&YYYY);
+	printf("Day-%d,Month-%d,Year-%d",DD,MM,YYYY);*/   
+//int x=printf("ineuron");
+	//printf(" %d",x);
+	//printf("\\n");
+	//printf("%d");//wrong
+	//float r,A;
+	//int length;
+	/*char l[30];
+	printf("Enter Your Name: ");//Like Suraj_Kumar_Kushavaha
+	scanf("%s",l);
+	printf("\"Hello, %s\"");*/
+//	printf("Hello Students");
+	//printf("Hello\nStudents");
+	//printf("\"MySirG\"");
+	/*printf("Enter Radius: ");
+	scanf("%f",&r);
+	A=3.14*r*r;
+	printf("Area of Circle is %.2f having Radius %.2f",A,r);*/
+	//length=printf("SURAJ");
+	//printf(" %d",length);
+	//printf("\"\\\""); //wrong required output is not printing output should be \\
+	return 0;
+}
